e6dc5df41d60bd5eb0b7d2231a06b16e8c0f5b37
Magnus Altgaard <magnus.altgard@biltema.com>
2023-12-07 09:57:34 +0100

    day 7 p2 wip

--- dotnet/aoc23/days/Day7.cs
+++ dotnet/aoc23/days/Day7.cs
@@ -8,7 +8,7 @@ public static class Day7
     {
         var input = FileReader.ReadAllLines(7).Select(r => r.Split(' ', StringSplitOptions.TrimEntries)).Select(s => (hand: s[0], bid: int.Parse(s[1])));
 
-        var comparer = new HandComparer();
+        var comparer = new HandComparer1();
 
         var sum = input.OrderBy(x => x.hand, comparer).Aggregate((sum: 0L, index: 1), (s, v) =>
         {
@@ -20,7 +20,7 @@ public static class Day7
         return sum.sum;
     }
 
-    public class HandComparer : IComparer<string>
+    internal class HandComparer1 : IComparer<string>
     {
         const string values = "AKQJT98765432";
         public int Compare(string x, string y)
@@ -46,9 +46,8 @@ public static class Day7
             return handCompare;
         }
 
-        public int GetStrength(string hand)
+        private static int GetStrength(string hand)
         {
-            ;
             var grouped = hand.GroupBy(c => c).ToList();
             return grouped switch
             {
@@ -67,5 +66,14 @@ public static class Day7
     public static int RunPart2()
     {
         return 0;
+    }
+    internal class HandComparer2 : IComparer<string>
+    {
+        const string values = "AKQT98765432J";
+
+        public int Compare(string? x, string? y)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
