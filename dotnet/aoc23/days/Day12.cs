d981335e1b6cbeba3b0dcf4a26c544937d460e0e
Magnus Altgård <magnus.altgard@outlook.com>
2023-12-11 22:36:09 +0100

    added placeholders

---
+++ dotnet/aoc23/days/Day12.cs
@@ -0,0 +1,25 @@
+using aoc23.helpers;
+
+public static class Day12
+{
+    public static int RunPart1()
+    {
+        var input = FileReader.ReadAllLines(12);
+
+        var total = 0;
+
+
+        return total;
+    }
+
+    public static int RunPart2()
+    {
+        var input = FileReader.ReadAllLines(12);
+
+        var total = 0;
+
+
+        return total;
+    }
+
+}
